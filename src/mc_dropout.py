5b4f3907fee77276eb637d79d253a3a53d44497f
Safaa Osman <s.o.abuzaid@gmail.com>
2026-09-24 11:04:55 +0300

    create run_mc_dropout function

--- src/mc_dropout.py
+++ src/mc_dropout.py
@@ -41,3 +41,49 @@ def mc_dropout_predict(model, images, mc_samples=30):
         predicted_classes,
         uncertainty
     )
+
+
+def run_mc_dropout(model, test_loader, device, mc_passes=30):
+
+    model = enable_mc_dropout(model)
+
+    results = []
+
+    with torch.no_grad():
+
+        for batch_idx, (images, labels) in enumerate(test_loader):
+
+            images = images.to(device)
+
+            mean_probabilities, predicted_classes, uncertainty = (
+                mc_dropout_predict(
+                    model,
+                    images,
+                    mc_samples=mc_passes
+                )
+            )
+
+            mean_probabilities = mean_probabilities.cpu().numpy()
+            predicted_classes = predicted_classes.cpu().numpy()
+            uncertainty = uncertainty.cpu().numpy()
+            labels = labels.numpy()
+
+            for i in range(len(labels)):
+
+                results.append({
+                    "image_id": batch_idx * test_loader.batch_size + i,
+                    "true_class": int(labels[i]),
+                    "predicted_class": int(predicted_classes[i]),
+                    "prob_class_0": float(mean_probabilities[i][0]),
+                    "prob_class_1": float(mean_probabilities[i][1]),
+                    "prob_class_2": float(mean_probabilities[i][2]),
+                    "uncertainty": float(uncertainty[i]),
+                    "correct": int(labels[i] == predicted_classes[i])
+                })
+
+            if (batch_idx + 1) % 10 == 0:
+                print(
+                    f"Processed {batch_idx + 1}/{len(test_loader)} batches"
+                )
+
+    return pd.DataFrame(results)
